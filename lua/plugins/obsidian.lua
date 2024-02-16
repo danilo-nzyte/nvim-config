8492045722fc3c1607551c313d7c30f919611bdf
danilo-nzyte <danilo.drobac@nzyte.co.uk>
2024-02-16 09:07:10 +0000

    first commit

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,17 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = false,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "Dro",
+				path = "/Users/danilodrobac/Library/Mobile Documents/iCloud~md~obsidian/Documents/Dro",
+			},
+		},
+	},
+}
