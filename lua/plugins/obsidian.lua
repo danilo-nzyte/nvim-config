a6454728f1a640656dced0c16d707f37d9c5d8f7
danilo-nzyte <danilo.drobac@nzyte.co.uk>
2024-02-20 19:11:03 +0000

    add obsidian frontmatter to notes

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -13,7 +13,7 @@ return {
 				path = "/Users/danilodrobac/Library/Mobile Documents/iCloud~md~obsidian/Documents/Dro",
 			},
 		},
-		notes_subdir = "fleeting-notes",
+		notes_subdir = "input",
 
 		-- Optional, customize how names/IDs for new notes are created.
 		note_id_func = function(title)
@@ -31,6 +31,25 @@ return {
 				end
 			end
 			return tostring(os.time()) .. "-" .. suffix
+		end,
+
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+
+			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			return out
 		end,
 
 		-- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
