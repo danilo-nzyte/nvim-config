f5b646ee4b1cd35c9ed34727e73b6ed2b4d00a4b
danilo-nzyte <danilo.drobac@nzyte.co.uk>
2024-03-09 08:49:45 +0000

    add script to auto close file once opened from neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,24 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
 	},
-		config = function()
-			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		end
-    }
-	
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+	end,
+}
